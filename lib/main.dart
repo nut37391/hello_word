da4c600399951f1d7ddc72d3c96155a8c4055116
Pattarapol Parakawong <pattarapol233@gmail.com>
2021-03-06 14:45:35 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      drawer: Drawer(
-        child: Text("Hello Drawer"),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Text("Hello Drawer"),
+        ),
+        appBar: AppBar(
+          title: Text("My First Project"),
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+            BottomNavigationBarItem(icon: Icon(Icons.phone), label: "phone"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard), label: "dashboard"),
+          ],
+        ),
       ),
-      appBar: AppBar(
-        title: Text("My first project"),
-      ),
-      body: Center(
-        child: Text("Hello world"),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-          label: "favorite"
-          ),
-          BottomNavigationBarItem(
-            icon:Icon(Icons.phone),
-          label: "phone"
-          ),
-          BottomNavigationBarItem(
-            icon:Icon(Icons.voicemail),
-          label: "voicemail"
-          ),
-        ],
-      ),
-    ),
-  ));
+    );
+  }
 }
